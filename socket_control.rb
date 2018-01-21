799534189dd01fbc5709a70083c8a7bb2d682608
Can Eldem <celdem@zendesk.com>
2018-01-21 22:21:52 +0000

    first version

---
+++ socket_control.rb
@@ -0,0 +1,57 @@
+require 'pi_piper'
+
+class Energenie
+  include PiPiper
+
+  ON_SIGNALS = {
+    0 => %i[on on off on],
+    1 => %i[on on on on],
+    2 => %i[off on on on],
+    3 => %i[on off on on],
+    4 => %i[off off on on]
+  }.freeze
+
+  OFF_SIGNALS = {
+    0 => %i[on on off off],
+    1 => %i[on on on off],
+    2 => %i[off on on off],
+    3 => %i[on off on off],
+    4 => %i[off off on off]
+  }.freeze
+
+  def initialize
+    setup_pins
+  end
+
+  # Pin numbers in this method comes from energenie manual
+  def setup_pins
+    @signal_pins = [17, 22, 23, 27].map do |pin|
+      PiPiper::Pin.new(pin: pin, direction: :out)
+    end
+
+    @on_off_pin = PiPiper::Pin.new(pin: 24, direction: :out)
+    @enable_pin = PiPiper::Pin.new(pin: 25, direction: :out)
+
+    @on_off_pin.off
+    @enable_pin.off
+
+    @signal_pins.map(&:off)
+  end
+
+  def change_plug_state(socket, signals_hash)
+    socket_signals = signals_hash[socket]
+    @signal_pins.zip(socket_signals).each { |pin, signal| pin.send(signal) }
+    sleep(0.1)
+    @enable_pin.on
+    sleep(0.25)
+    @enable_pin.off
+  end
+
+  def switch_on(socket = 0)
+    change_plug_state(socket, ON_SIGNALS)
+  end
+
+  def switch_off(socket = 0)
+    change_plug_state(socket, OFF_SIGNALS)
+  end
+end
